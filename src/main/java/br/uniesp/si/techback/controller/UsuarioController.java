9f8214c79276b20407cf39b400a17fa868396ddc
Manoeljustino <manoeljustino@gmail.com>
2025-03-18 21:23:50 -0300

    commit aula 18/03

--- src/main/java/br/uniesp/si/techback/controller/UsuarioController.java
+++ src/main/java/br/uniesp/si/techback/controller/UsuarioController.java
@@ -1,25 +1,37 @@
 package br.uniesp.si.techback.controller;
 
+import br.uniesp.si.techback.model.Usuario;
+import br.uniesp.si.techback.service.UsuarioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
-    private UsurioService usuarioservice;
+    @Autowired
+    private UsuarioService usuarioservice;
 
-    public Usuario criarUsuario(Usuario usuario) {
-        return null;
+    @PostMapping
+    public Usuario incluir(Usuario usuario) {
+        return usuarioservice.cadastrar(usuario);
+    }
+    @GetMapping
+    public List<Usuario> listar() {
+        return usuarioservice.listar();
     }
 
-    public List<Usuario> listarUsuarios() {
-        return null;
+    @PutMapping
+    public Usuario alterar (Usuario usuario) {
+        return usuarioservice.alterar(usuario);
     }
 
-    public Usuario buscarUsuario(Long id) {
-        return null;
-    }
-
-    public Usuario atualizarUsuario(Long id, Usuario usuario) {
-        return null;
-    }
-
-    public void deletarUsuario(Long id) {
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(Integer id) {
+        usuarioservice.excluir(id);
+        return ResponseEntity.ok().build();
 
     }
 }
